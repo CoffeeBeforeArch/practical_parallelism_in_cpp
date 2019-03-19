184a324a96cd053f118f7521df14ae8e9f2f4ce1
CoffeeBeforeArch <CoffeeBeforeArch@gmail.com>
2019-03-18 20:18:09 -0400

    Add intro folder

---
+++ intro/intro/call_from.cpp
@@ -0,0 +1,30 @@
+// This program is a multi-threaded "Hello, World!" program in C++
+// By: Nick from CoffeeBeforeArch
+
+#include <iostream>
+#include <thread>
+
+// Initial function for each thread
+void call_from(int tid) {
+	std::cout << "Launched by thread " << tid << std::endl;
+}
+
+int main() {
+	// Create an array of 10 thread objects
+	std::thread t[10];
+
+	// Launch 10 threads with initial function "call_from(...)"
+	for (int i = 0; i < 10; i++) {
+		t[i] = std::thread(call_from, i);
+	}
+
+	// Print from main function
+	std::cout << "Launched from main" << std::endl;
+
+	// Wait for all threads to complete
+	for (int i = 0; i < 10; i++) {
+		t[i].join();
+	}
+
+	return 0;
+}
